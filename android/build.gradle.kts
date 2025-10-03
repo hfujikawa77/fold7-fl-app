594c1e5e7295a1250ba38d8dfb565ae0ae20f67f
Hideyuki Fujikawa <hfujikawa77@gmail.com>
2025-10-03 09:50:44 +0900

    ヒンジ角度表示機能追加

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,6 +3,32 @@ allprojects {
         google()
         mavenCentral()
     }
+    afterEvaluate {
+        if (project.name == "dual_screen") {
+            try {
+                the<com.android.build.gradle.BaseExtension>().apply {
+                    if (namespace == null) {
+                        namespace = "com.example.dual_screen"
+                        println("WARNING: Manually setting namespace for project 'dual_screen' to '$namespace'")
+                    }
+                    compileOptions {
+                        sourceCompatibility = JavaVersion.VERSION_1_8
+                        targetCompatibility = JavaVersion.VERSION_1_8
+                    }
+                }
+                project.extensions.findByType(org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions::class.java)?.apply {
+                    jvmTarget = "1.8"
+                }
+                project.tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+                    kotlinOptions {
+                        jvmTarget = "1.8"
+                    }
+                }
+            } catch (e: Exception) {
+                // Ignore projects that don't have the android extension.
+            }
+        }
+    }
 }
 
 val newBuildDir: Directory =
